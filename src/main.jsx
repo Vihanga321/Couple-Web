ce4deefb39b1779aadd92df54dfbf95c34dfb668
Mr.Oska <vihangamihisaraappuhamy@gmail.com>
2026-08-10 03:36:46 +0530

    feat: switch app entry to complete Twonara V1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './AppPhase3';
+import App from './AppFinal';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
