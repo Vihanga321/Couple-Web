0730aa493a91667ade0ad4c3307727a1b23ab32d
Mr.Oska <vihangamihisaraappuhamy@gmail.com>
2026-08-12 13:44:35 +0530

    feat: switch Twonara to public customer shop architecture

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './AppMarketplace';
+import App from './AppMarketplaceV2';
 import HomeLocationDropdown from './components/HomeLocationDropdown';
 import './styles.css';
 import './mobile-fixes.css';
 import './auth-google.css';
 import './auth-3d-fixes.css';
+import './guest-stories.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
